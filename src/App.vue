95ee7327d9631f87b712198856e7084db44c2c70
Ivan Švaljek <ivan.svaljek@gmail.com>
2020-08-25 07:14:06 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >      
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-app-bar-nav-icon @click.stop="showDrawer = !showDrawer"></v-app-bar-nav-icon>
+      
+    </v-app-bar>
+
+    <v-main>
+      <v-navigation-drawer v-model="showDrawer" absolute>
+        <v-checkbox v-for="(item, i) in checkboxes" :key="i" label="Clicker"></v-checkbox>
+        <br />
+      </v-navigation-drawer>
+      <HelloWorld/>
+      <Lipsum v-for="(item, i) in lipsums" :key="i" />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
+import Lipsum from './components/Lipsum';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+    Lipsum
+  },
+
+  computed: {
+    json_kozmo() {
+      return this.kozmo
+    }
+  },
+
+  async mounted() {
+    
+    
+  },
+
+  data: () => ({
+    checkboxes: Array(100),
+    lipsums: Array(5),
+    showDrawer: false,
+  }),
+};
+</script>
---
+++ src/components/Lipsum.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container>
+    <div id="lipsum">
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra rutrum arcu at lacinia. Donec interdum placerat sem eu faucibus. Nam finibus pulvinar risus, at ullamcorper ligula molestie ac. Sed viverra odio ligula, vitae euismod felis suscipit volutpat. Aliquam a neque in quam accumsan ornare. Proin at purus ex. Aenean lacinia eu nulla vel eleifend. Nulla venenatis risus sit amet velit facilisis efficitur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec est urna, maximus eu ultrices a, ultricies sed lectus.
+</p>
+<p>
+Nullam eget massa lacus. In sem neque, pulvinar in rutrum id, tristique quis lacus. Fusce neque purus, sagittis vel iaculis sed, porta quis dolor. Pellentesque consectetur erat at fermentum convallis. Aenean congue magna eu odio sodales egestas. Integer in dapibus neque. Vestibulum viverra euismod diam in condimentum. Maecenas ac enim sit amet lectus ullamcorper vulputate. Nam finibus sit amet lorem vel varius. Praesent vitae egestas quam. Nullam odio velit, semper a sapien eu, mattis cursus felis. Suspendisse potenti.
+</p>
+<p>
+Praesent a est gravida, pharetra tellus id, auctor odio. Quisque faucibus nunc vel justo suscipit, vitae tincidunt quam scelerisque. Nam lectus magna, malesuada vitae ligula eget, porta feugiat ipsum. Vivamus non sodales sem. Donec blandit orci felis, vitae feugiat ante faucibus non. Ut a nibh ac justo consectetur interdum nec et nibh. Nunc ac varius ligula. Proin venenatis, lorem sit amet ultricies pharetra, urna diam pharetra sapien, vitae consectetur enim nibh eget felis. Cras congue congue sollicitudin. Quisque ipsum ante, maximus nec finibus id, aliquam id dolor.
+</p>
+<p>
+Phasellus vitae malesuada arcu, at aliquet metus. Nulla id augue commodo justo bibendum laoreet. Curabitur viverra sapien arcu, ut feugiat orci vestibulum ac. Vestibulum ornare id leo quis blandit. Sed congue justo metus, et ultricies urna pretium a. Suspendisse scelerisque vehicula libero, eget tincidunt ex malesuada id. Pellentesque pharetra bibendum purus, nec sagittis risus condimentum eu.
+</p>
+<p>
+Suspendisse potenti. Praesent sit amet ullamcorper diam. In ultricies enim non nisi molestie fermentum. Donec mattis ex nibh, eu auctor nisi rutrum nec. Suspendisse orci odio, volutpat ut convallis ac, accumsan eu sapien. Aenean sed hendrerit erat. Curabitur vitae risus eget felis volutpat hendrerit. Curabitur eu feugiat dui. Vivamus placerat dictum commodo. Nulla facilisi. Sed nec ultricies lorem, vitae aliquet nibh. Fusce semper lacus purus, ac placerat ex varius et.
+</p>
+<p>
+Sed at imperdiet ex, ut congue felis. In eget justo pretium, varius risus quis, egestas turpis. Etiam aliquet finibus orci non scelerisque. Morbi vitae eros turpis. Maecenas fringilla dictum mollis. Quisque ut dapibus ante. In sed lacus consectetur, accumsan felis at, pellentesque dolor. Nam sed tempus metus, a lobortis leo. Sed congue leo id aliquet placerat. Etiam tincidunt felis at arcu eleifend commodo. Integer nec nulla sit amet massa rhoncus rhoncus tristique id libero.
+</p>
+<p>
+Curabitur aliquet ex neque, non semper sem euismod in. Aliquam tincidunt lectus vitae felis posuere dignissim. Nullam vestibulum tincidunt elementum. Sed consectetur consequat mi, id laoreet neque euismod sed. In vulputate, velit eu aliquam pretium, ante ante posuere augue, sit amet ullamcorper neque massa nec justo. Fusce non est purus. Nunc non nibh eros. Aliquam erat volutpat.
+</p>
+<p>
+Cras tempus tempor dui, non efficitur nisi euismod sed. Vestibulum egestas, dolor eget viverra lobortis, ligula nibh porta ligula, at sagittis metus sem eu velit. Aliquam erat volutpat. Phasellus ut velit et sapien congue euismod vitae mattis lectus. Donec eget placerat tortor. Sed ornare dictum lobortis. Sed ac nisi sit amet eros euismod aliquam.
+</p>
+<p>
+Donec viverra velit massa, a porttitor quam varius id. Integer consequat consequat rhoncus. Aenean rutrum lacus purus, eget venenatis sem maximus quis. Proin dapibus interdum aliquet. Cras bibendum massa augue. Suspendisse sagittis mauris et enim facilisis, sed ornare lacus dictum. Proin in risus condimentum, dapibus nisl sed, vulputate sem. Phasellus quis ipsum tristique, consectetur metus ac, bibendum neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+</p>
+<p>
+Proin tincidunt eu tellus non molestie. Donec nec finibus ligula. Pellentesque nisl felis, elementum eu augue vitae, ultrices varius nulla. Morbi non augue quis eros finibus hendrerit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum mattis nibh vel sem ullamcorper blandit. Quisque suscipit non lectus sed commodo.
+</p>
+<p>
+Nullam et diam lacinia purus porta aliquam ut ut sapien. Maecenas ac dolor felis. Suspendisse nulla sem, pulvinar sit amet dui quis, consectetur finibus ipsum. Proin aliquam purus sem, sit amet venenatis augue tincidunt eu. Vestibulum vel sagittis mauris. Sed et dapibus risus. Sed iaculis, dolor ut ullamcorper lobortis, enim massa sagittis quam, at ornare tellus risus ut arcu.
+</p>
+<p>
+Sed nunc neque, convallis in felis vel, facilisis finibus purus. Etiam a arcu nec eros molestie elementum. Proin tempor a metus quis porta. Nunc purus enim, sollicitudin vitae porttitor at, laoreet id odio. Nullam semper nisl nec lobortis commodo. Sed elit mauris, accumsan non finibus at, tempor id erat. Etiam dapibus scelerisque diam id scelerisque. Curabitur viverra quam ac orci finibus lobortis. Aenean et ex gravida, ornare quam nec, tincidunt est. Quisque dapibus est dapibus elit fringilla ullamcorper. Nunc sed lacus in risus aliquet porttitor. Curabitur faucibus sem et pretium feugiat. Integer in consequat sapien.
+</p>
+<p>
+Aliquam consequat massa eget libero bibendum rhoncus sit amet quis mi. Proin vulputate velit sodales purus rutrum, quis mollis quam dapibus. Aliquam nibh ante, dignissim eu ipsum ut, semper dignissim erat. Donec id maximus ex, non mattis mi. Maecenas ullamcorper dui id vehicula finibus. Ut vel sapien vitae augue varius mollis. Etiam pellentesque felis tristique scelerisque tincidunt. Suspendisse pretium eget erat a gravida. Morbi pharetra nulla in tincidunt egestas. Nunc rhoncus ex in elit bibendum, ut volutpat purus placerat. Vivamus consequat non elit euismod molestie. Nullam a enim in magna mattis dictum vitae sit amet urna. Sed molestie consequat est, vel mollis nisl viverra a. Integer vel nisi nec mauris placerat pulvinar.
+</p>
+<p>
+Phasellus sit amet porttitor est. Suspendisse nec interdum odio. Ut ornare tempor ex a rutrum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce vehicula sapien at libero laoreet consectetur. Sed sodales porttitor nibh in fringilla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse accumsan dignissim diam et mattis. In nec auctor elit, a imperdiet quam. Nam arcu mauris, feugiat eget augue et, cursus lacinia purus.
+</p>
+<p>
+Nunc lorem mauris, consectetur sit amet est eu, consequat tempor erat. Maecenas in vulputate magna, sed egestas urna. Nulla efficitur ornare pellentesque. Donec porttitor aliquam odio sed accumsan. Morbi a nisl eget libero sodales gravida. Sed faucibus commodo ultrices. Fusce nec libero orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis lectus in turpis faucibus, vehicula fringilla arcu facilisis. Nulla facilisi. Etiam quis molestie ex.
+</p>
+<p>
+Donec nunc quam, pharetra eu consectetur at, viverra non purus. Praesent ac malesuada dui, sit amet interdum lacus. Phasellus id ex vitae mi ultrices molestie quis a nisl. Integer imperdiet feugiat lorem vel maximus. Sed varius vehicula erat, id bibendum lacus auctor ultricies. Duis maximus massa quis massa convallis vestibulum. In hac habitasse platea dictumst. Vestibulum pretium magna dolor, quis suscipit lorem congue rhoncus. Cras id turpis nec libero tincidunt gravida eu ac nisl. Fusce ut leo fringilla, euismod sapien vitae, tincidunt arcu. Phasellus sagittis arcu ultrices blandit lobortis. Phasellus tristique neque in sapien euismod blandit. Suspendisse non leo nec justo condimentum facilisis.
+</p>
+<p>
+Pellentesque ut ipsum vitae elit placerat faucibus. Donec ullamcorper arcu at turpis viverra aliquam. Nunc fringilla tortor ac odio tincidunt, non condimentum enim mollis. Sed nulla enim, aliquam id lobortis at, tempor ut nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas molestie enim vestibulum iaculis aliquam. Sed at eleifend odio, non cursus magna. Nam euismod sem posuere, porta metus at, ultrices enim. Duis vel odio augue. Nam porta orci vel lobortis mattis.
+</p>
+<p>
+Vestibulum et sapien facilisis, luctus urna sed, ultricies eros. Ut lacinia ligula tellus, in euismod dolor faucibus vitae. Curabitur non ante enim. Nulla posuere metus laoreet libero varius, non gravida tellus vehicula. Sed ullamcorper porttitor egestas. Maecenas eget erat felis. Pellentesque vitae ullamcorper magna, et finibus orci. Maecenas a leo sed mauris rhoncus fringilla. Curabitur fringilla metus vel auctor pulvinar. Sed vitae metus tempor, lobortis enim vitae, auctor ipsum. Fusce nec tellus risus. Vivamus porttitor massa at diam euismod consectetur. Sed vitae lacus nec nulla placerat sodales.
+</p>
+<p>
+Maecenas sapien leo, eleifend ac laoreet sed, venenatis sed nisi. Etiam bibendum, tellus quis efficitur malesuada, erat nisi tristique leo, nec finibus dolor nisi porttitor felis. Donec pharetra sagittis quam sed cursus. Quisque a lacinia felis, nec commodo libero. Nam semper eleifend urna in placerat. Quisque lacinia, leo hendrerit mollis laoreet, quam dui elementum velit, in congue purus est id eros. Aenean risus dolor, blandit at malesuada a, suscipit quis purus. Nullam nunc ipsum, malesuada a pretium a, tempus vitae lacus. Morbi volutpat enim nec ante volutpat fringilla. Aliquam non magna quis nisi lobortis sodales. Donec eget hendrerit mi. Donec quis purus non ipsum mollis congue.
+</p>
+<p>
+Donec eleifend vitae lacus quis vestibulum. Nunc sed placerat nibh, vel suscipit tortor. Duis blandit metus a lacinia rhoncus. Sed interdum facilisis massa, ac vehicula lectus placerat vestibulum. Nunc ultricies diam a ex placerat, nec scelerisque nunc volutpat. In ex ante, condimentum eu dui non, pharetra viverra mi. Donec id sapien quis enim blandit fermentum. Curabitur ac iaculis felis.
+</p></div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Lipsum',    
+  }
+</script>
